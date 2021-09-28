a1655be016c5acc22864b3f260a1dfb63ceffb7e
dmprytul <dmprytul@microsoft.com>
2021-09-28 17:15:47 +0200

    bla bla

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -1,7 +1,11 @@
-import java.io.*;
-import java.nio.file.*;
-import java.util.*;
-import java.util.stream.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MainApplication {
 
@@ -20,17 +24,29 @@ public class MainApplication {
 
         System.out.println("У нас на сегодня " + tasks.size() + " актуальных сверхзадач");
         tasks.sort((x, y) -> {
-            amountOfComparing++;
-            System.out.println("--------------------------");
-            System.out.println("Что нужно сделать быстрее?");
-            System.out.println("Или что важнее?");
-            System.out.println("1. " + x);
-            System.out.println("2. " + y);
-            return scanner.nextInt() == 1 ? -1 : 1;
+            int compare;
+            while (true) {
+                amountOfComparing++;
+                System.out.println("--------------------------");
+                System.out.println("Что нужно сделать быстрее?");
+                System.out.println("Или что важнее?");
+                System.out.println("1. " + x);
+                System.out.println("2. " + y);
+                compare = scanner.nextInt();
+                if (compare == 1 || compare == 2) {
+                    break;
+                } else {
+                    System.out.println("Не правильный агрумент. Допустимые {1, 2}.");
+                }
+            }
+            if (compare == 0) {
+                throw new RuntimeException("Unreachable statement");
+            }
+            return compare == 1 ? -1 : 1;
         });
         System.out.println("В этот раз у нас " + amountOfComparing + " сравнений!");
 
-        try (PrintWriter writer = new PrintWriter(new File("output.txt"))) {
+        try (PrintWriter writer = new PrintWriter("output.txt")) {
             tasks.forEach(writer::println);
         }
 
